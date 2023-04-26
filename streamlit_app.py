1c33f59ca9a9f669cd3e3650cc0285a9e5f2bea7
aborse555 <116681133+aborse555@users.noreply.github.com>
2023-04-26 18:46:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
-from urllib.error import URLError
+# import snowflake.connector
+# from urllib.error import URLError
 
 streamlit.title('SHPM')
