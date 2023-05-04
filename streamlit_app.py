ca537406ed345fe7cf7e721dd703fa2ee6cd2233
aborse555 <116681133+aborse555@users.noreply.github.com>
2023-05-04 17:26:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,16 @@ from urllib.error import URLError
 
 streamlit.title('SHPM')
 streamlit.header('Basic Information')
-def get_ssn_infor():
+streamlit.header("Enter SSN Here with No Dashes")
+try:
+  ssn_choice = streamlit.text_input('What is your SSN?')
+  if not ssn_choice:
+    streamlit.error("Please enter a SSN to get information.")
+  else:
+    back_from_function = get_ssn_info(ssn_choice)
+    streamlit.dataframe(back_from_function)
+
+def get_ssn_info():
   with my_cnx.cursor() as my_cur:
        my_cur.execute("select * from TMEM where SSN = ('" + ssn_choice + "')")
        return my_cur.fetchall()
@@ -16,15 +25,6 @@ if streamlit.button('Get Basic Information'):
   my_data_rows = get_ssn_info()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
-
-streamlit.header("Enter SSN Here with No Dashes")
-try:
-  ssn_choice = streamlit.text_input('What is your SSN?')
-  if not ssn_choice:
-    streamlit.error("Please enter a SSN to get information.")
-  else:
-    back_from_function = get_ssn_info(ssn_choice)
-    streamlit.dataframe(back_from_function)
 
 except URLError as e: 
   streamlit.error()
