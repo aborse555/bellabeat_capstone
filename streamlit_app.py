aac518cd6be914d7a2a38cc7d2497d781a2ef984
aborse555 <116681133+aborse555@users.noreply.github.com>
2023-05-04 14:36:27 -0400

    Update streamlit_app.py
    added code to call snowflake information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,34 @@
+# don't run anything from this line while troubleshooting
+streamlit.stop()
+
 import streamlit
 import pandas
 import requests
-# import snowflake.connector
+import snowflake.connector
 # from urllib.error import URLError
 
 streamlit.title('SHPM')
+
+def get_ssn_infor():
+  with my_cnx.cursor() as my_cur:
+       my_cur.execute("select * from TMEM where SSN = ('" + ssn_choice + "')")
+       return my_cur.fetchall()
+
+if streamlit.button('Get Basic Information'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_ssn_info()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
+
+streamlit.header("Enter SSN Here with No Dashes")
+try:
+  ssn_choice = streamlit.text_input('What is your SSN?')
+  if not ssn_choice:
+    streamlit.error("Please enter a SSN to get information.")
+  else:
+    back_from_function = get_ssn_infor(ssn_choice)
+    streamlit.dataframe(back_from_function)
+
 streamlit.header('Basic Information')
 streamlit.header('Comments')
 streamlit.header('Dependents')
