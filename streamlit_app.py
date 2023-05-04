5351862e089b96ad79013046e3c7ee195832c584
aborse555 <116681133+aborse555@users.noreply.github.com>
2023-05-04 18:00:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,24 +11,29 @@ def get_ssn_info(ssn_choice):
        my_cur.execute("select * from TMEM where SSN = ('" + ssn_choice + "')")
        return my_cur.fetchall()
   
-streamlit.header("Enter SSN Here with No Dashes")
-try:
-  ssn_choice = streamlit.text_input('What is your SSN?')
-  if not ssn_choice:
-    streamlit.error("Please enter a SSN to get information.")
-  else:
-    back_from_function = get_ssn_info(ssn_choice)
-    streamlit.dataframe(back_from_function)      
-
-except URLError as e:
-  streamlit.error()    
-
-if streamlit.button('Get Basic Information'):
+my_ssn = streamlit.text_input('What is your SSN?')
+if streamlit.button('Enter Your SSN'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_data_rows = get_ssn_info()
+  back_from_function = get_ssn_info(my_ssn)
+  streamlit.text(back_from_function)
   my_cnx.close()
-  streamlit.dataframe(my_data_rows)
 
+#if streamlit.button('Get Basic Information'):
+#  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+ # my_data_rows = get_ssn_info()
+  #my_cnx.close()
+  #streamlit.dataframe(my_data_rows)
+
+#streamlit.header("Enter SSN Here with No Dashes")
+#try:
+ # ssn_choice = streamlit.text_input('What is your SSN?')
+  #if not ssn_choice:
+   # streamlit.error("Please enter a SSN to get information.")
+  #else:
+   # back_from_function = get_ssn_info(ssn_choice)
+    #streamlit.dataframe(back_from_function)      
+#except URLError as e:
+ # streamlit.error()    
 # streamlit.header('Comments')
 # streamlit.header('Dependents')
 # streamlit.header('Coverage History')
