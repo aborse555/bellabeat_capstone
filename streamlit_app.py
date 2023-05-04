c83b14c67182e7f689ec1b9ebdbaf2c4e7c0dd2c
aborse555 <116681133+aborse555@users.noreply.github.com>
2023-05-04 14:21:25 -0400

    Update streamlit_app.py
    added all subgroups

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,11 @@ import requests
 # from urllib.error import URLError
 
 streamlit.title('SHPM')
+streamlit.header('Basic Information')
+streamlit.header('Comments')
+streamlit.header('Dependents')
+streamlit.header('Coverage History')
+streamlit.header('Medicare Information')
+streamlit.header('Member Group Summary')
+streamlit.header('Direct Pay History')
+streamlit.header('Direct Premium Status')
