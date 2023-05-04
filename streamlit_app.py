bdf4a3fb3e4f8bc7d75bf65c48fa4a84c0e3d4dd
aborse555 <116681133+aborse555@users.noreply.github.com>
2023-05-04 17:53:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,31 +23,11 @@ try:
 except URLError as e:
   streamlit.error()    
 
-
-#streamlit.title('SHPM')
-#streamlit.header('Basic Information')
-#streamlit.header("Enter SSN Here with No Dashes")
-#try:
- # ssn_choice = streamlit.text_input('What is your SSN?')
-  #if not ssn_choice:
-   # streamlit.error("Please enter a SSN to get information.")
-  #else:
-   # back_from_function = get_ssn_info(ssn_choice)
-    #streamlit.dataframe(back_from_function)      
-
-#def get_ssn_info():
-  #with my_cnx.cursor() as my_cur:
-      # my_cur.execute("select * from TMEM where SSN = ('" + ssn_choice + "')")
-       #return my_cur.fetchall()
-
-#if streamlit.button('Get Basic Information'):
- # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  #my_data_rows = get_ssn_info()
-  #my_cnx.close()
-  #streamlit.dataframe(my_data_rows)
-
-#except URLError as e: 
- # streamlit.error()
+if streamlit.button('Get Basic Information'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_ssn_info()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
 
 # streamlit.header('Comments')
 # streamlit.header('Dependents')
